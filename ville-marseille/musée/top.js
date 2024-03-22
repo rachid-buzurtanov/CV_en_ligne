4c9987dfb03960b156ed967cb3eb8f12e7bc394c
rachid-buzurtanov <146711214+rachid-buzurtanov@users.noreply.github.com>
2024-03-22 16:36:26 +0100

    Delete ville-marseille directory

--- ville-marseille/musée/top.js
+++
@@ -1,9 +0,0 @@
-const menu = document.querySelector('.menu');
-
-menu.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth"
-    })
-})
